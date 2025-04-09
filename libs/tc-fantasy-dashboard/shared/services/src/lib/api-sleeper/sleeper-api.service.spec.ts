066e65add3942f6f54a48b8fad6c7b198f9bcd1f
jbmitchell7 <coding+github@thundercloud.dev>
2025-04-09 02:11:05 +0000

    adds unit tests for sleeper api

--- libs/tc-fantasy-dashboard/shared/services/src/lib/api-sleeper/sleeper-api.service.spec.ts
+++ libs/tc-fantasy-dashboard/shared/services/src/lib/api-sleeper/sleeper-api.service.spec.ts
@@ -1,21 +1,104 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SleeperApiService } from './sleeper-api.service';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import {
+  mockDraft,
+  mockLeagueResponse,
+  mockManager,
+  mockRoster,
+  mockSportState,
+  mockTransaction,
+} from '@tc-fantasy-dashboard/shared/mock-data';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  Manager,
+  Roster,
+  Transaction,
+} from '@tc-fantasy-dashboard/shared/interfaces';
 
-describe('SleeperApiService', () => {
+describe('FantasyDashboardApiService', () => {
   let service: SleeperApiService;
+  let httpMock: HttpTestingController;
+  const apiUrl = 'https://api.sleeper.app/v1';
+  const leagueId = mockLeagueResponse.league_id;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [],
-    providers: [provideHttpClient(), provideHttpClientTesting()]
-});
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(SleeperApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch league data', () => {
+    service.getLeague(leagueId).subscribe((response) => {
+      expect(response).toEqual(mockLeagueResponse);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/league/${leagueId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockLeagueResponse);
+  });
+
+  it('should fetch rosters', () => {
+    const mockRosters = [mockRoster] as Roster[];
+    service.getRosters(leagueId).subscribe((response) => {
+      expect(response).toEqual(mockRosters);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/league/${leagueId}/rosters`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockRosters);
+  });
+
+  it('should fetch managers', () => {
+    const mockManagers = [mockManager] as Manager[];
+    service.getManagers(leagueId).subscribe((response) => {
+      expect(response).toEqual(mockManagers);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/league/${leagueId}/users`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockManagers);
+  });
+
+  it('should fetch sport state', () => {
+    service.getSportState('nfl').subscribe((response) => {
+      expect(response).toEqual(mockSportState);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/state/nfl`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockSportState);
+  });
+
+  it('should fetch transactions', () => {
+    const mockTransactions = [mockTransaction] as Transaction[];
+    service.getTransactions(leagueId, 1).subscribe((response) => {
+      expect(response).toEqual(mockTransactions);
+    });
+
+    const req = httpMock.expectOne(
+      `${apiUrl}/league/${leagueId}/transactions/1`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTransactions);
+  });
+
+  it('should fetch draft data', () => {
+    service.getDraft('1').subscribe((response) => {
+      expect(response).toEqual(mockDraft);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/draft/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockDraft);
   });
 });
