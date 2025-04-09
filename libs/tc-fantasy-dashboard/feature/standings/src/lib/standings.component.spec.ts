ca0fc0ae87fe34d7f16ad21b1a6cfac4a3a55bea
jbmitchell7 <coding+github@thundercloud.dev>
2025-04-09 23:48:55 +0000

    adds unit tests for league, standings, welcome

--- libs/tc-fantasy-dashboard/feature/standings/src/lib/standings.component.spec.ts
+++ libs/tc-fantasy-dashboard/feature/standings/src/lib/standings.component.spec.ts
@@ -2,17 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StandingsComponent } from './standings.component';
 import { provideHttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { LeagueInitService } from '@tc-fantasy-dashboard/shared/services';
+import { mockLeague } from '@tc-fantasy-dashboard/shared/mock-data';
+import { of } from 'rxjs';
 
 describe('StandingsComponent', () => {
   let component: StandingsComponent;
   let fixture: ComponentFixture<StandingsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-    imports: [StandingsComponent],
-    providers: [provideHttpClient(), MessageService]
-})
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [StandingsComponent],
+      providers: [
+        provideHttpClient(),
+        MessageService,
+        {
+          provide: LeagueInitService,
+          useValue: {
+            selectedLeague$: of(mockLeague),
+          },
+        }
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,7 +32,32 @@ describe('StandingsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set gridStyle based on mobileDevice', () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockReturnValue('true');
+    component.ngOnInit();
+    expect(component.gridStyle).toBe('p-datatable-striped p-datatable-sm');
+  });
+
+  it('should update standingsData and other properties on league selection', () => {
+    expect(component.standingsData).toEqual([
+      {
+        "avatarUrl": "",
+        "losses": 0,
+        "maxPoints": 0,
+        "owner_id": "123",
+        "playerIds": [],
+        "points": 0,
+        "pointsAgainst": 0,
+        "streak": "",
+        "streakColor": undefined,
+        "streakIcon": undefined,
+        "username": "",
+        "wins": 0,
+      },
+    ]);
+    expect(component.seasonStarted).toBeFalsy();
+    expect(component.leagueName).toBe(mockLeague.name);
+    expect(component.leagueYear).toBe(mockLeague.season);
+    expect(component.pageTitle).toBe(`${mockLeague.season} Standings`);
   });
 });
