22df4e92a961acb44133667686807d362599856a
jbmitchell7 <coding+github@thundercloud.dev>
2025-04-16 22:55:01 +0000

    adds accordion to roster panels for starters/bench/taxi

--- libs/tc-fantasy-dashboard/feature/rosters/src/lib/roster-card/roster-card.component.ts
+++ libs/tc-fantasy-dashboard/feature/rosters/src/lib/roster-card/roster-card.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { League, Manager, Player, Roster } from '@tc-fantasy-dashboard/shared/interfaces';
-import { PanelModule } from 'primeng/panel';
 import { getManager } from '@tc-fantasy-dashboard/shared/utils';
 import { PositionBadgeComponent } from '@tc-fantasy-dashboard/shared/components';
-import { map } from 'rxjs/operators';
+import { mockPlayer } from '@tc-fantasy-dashboard/shared/mock-data';
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'fd-roster-card',
-  imports: [CommonModule, PanelModule, PositionBadgeComponent],
+  imports: [CommonModule, AccordionModule, PositionBadgeComponent, PanelModule],
   templateUrl: './roster-card.component.html',
   styleUrl: './roster-card.component.css',
 })
@@ -18,15 +19,22 @@ export class RosterCardComponent implements OnInit {
   @Input({required: true}) league!: League;
 
   manager!: Manager;
-  rosteredPlayers!: Player[];
+  starters!: Player[];
+  bench!: Player[];
+  taxi!: Player[];
 
   ngOnInit(): void {
     this.manager = getManager(this.league, this.roster.roster_id) ?? ({} as Manager);
-    this.#getRosteredPlayers();
+
+    const benchedIds = this.roster.players
+      .filter(playerId => !this.roster.starters.includes(playerId) && !this.roster.taxi.includes(playerId));
+
+    this.bench = this.#getPlayerDataList(benchedIds);
+    this.starters = this.#getPlayerDataList(this.roster.starters);
+    this.taxi = this.#getPlayerDataList(this.roster.taxi);
   }
 
-  #getRosteredPlayers(): void {
-    const players = this.roster.players.map((playerId) => this.players[playerId]);
-    this.rosteredPlayers = players;
+  #getPlayerDataList(playerIds: string[]): Player[] {
+    return playerIds.map(playerId => this.players[playerId] ?? mockPlayer);
   }
 }
