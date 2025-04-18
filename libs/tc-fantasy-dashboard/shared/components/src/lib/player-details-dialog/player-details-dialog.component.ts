defd64af62db47528c5393e86652c5af180431f6
jbmitchell7 <coding+github@thundercloud.dev>
2025-04-18 01:02:08 +0000

    fixes console error issue

--- libs/tc-fantasy-dashboard/shared/components/src/lib/player-details-dialog/player-details-dialog.component.ts
+++ libs/tc-fantasy-dashboard/shared/components/src/lib/player-details-dialog/player-details-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Player } from '@tc-fantasy-dashboard/shared/interfaces';
 import { DialogModule } from 'primeng/dialog';
@@ -9,10 +9,10 @@ import { DialogModule } from 'primeng/dialog';
   templateUrl: './player-details-dialog.component.html',
 })
 export class PlayerDetailsDialogComponent {
-  player = input.required<Player>();
-  visible = false;
+  player = input<Player>();
+  readonly visible = signal<boolean>(false);
 
   constructor() {
-    effect(() => this.visible = !!this.player);
+    effect(() => this.visible.set(!!this.player()?.full_name));
   }
 }
